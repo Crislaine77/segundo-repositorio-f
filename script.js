8ff9364de1170f44c97670dc602f282e38e40b9b
Crislaine77 <crislaine.santos.barbosa@escola.pr.gov.br>
2025-07-02 21:26:04 -0300

    02/07/25

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {PALAVRAS_RUINS} from"./palavrasRuins.js
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
@@ -16,29 +17,35 @@ function processaTexto(texto) {
     //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
     let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
 
-  const frequencias = contaFrequencias(palavras);
+    const frequencias = contaFrequencias(palavras);
 
-  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-  function ordenaPalavra(p1, p2) {
-    return frequencias
-  }
-  
-    let frequencias = [];
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+    return ordenadas.slice(0,10);
+}
 
- for ( let i in palavras) {
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for ( let i of palavras) {
         frequencias[i] = 0;
-        for ( let j in palavras) {
-            if(palavras[i] == palavras[j]) {
+        for ( let j of palavras) {
+            if( i == j ) {
                 frequencias[i]++;
             }
         }
     }
-
-    console.log(frequencias);
-
     return palavras;
 }
 
-function ordenaPalavra(p1, p2) {
-    return frequencias [p2] - frequencias[p1]
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new setInterval(["para", "uma", "nós"]); //tem muito mais!!
+    const palavrasBoas =[];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
